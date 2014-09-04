88f9e7cfda39a6fb9c908312484982f8fc7ee727
Roman Steiner <roman@toastlab.ch>
2014-09-04 17:14:44 +0200

    oh f*ck

--- obj2pb.js
+++ obj2pb.js
@@ -9,9 +9,8 @@ var filenames = process.argv.slice(2);
 
 var Model = builder.build('Model');
 var Mesh = Model.Mesh;
-var Geometry = Mesh.Geometry;
-var Vector3 = Geometry.Vector3;
-var Face = Geometry.Face;
+var Vector3 = Model.Vector3;
+var Face = Mesh.Face;
 
 var vertex_pattern = /v( +[\d|\.|\+|\-|e]+)( +[\d|\.|\+|\-|e]+)( +[\d|\.|\+|\-|e]+)/;
 var normal_pattern = /vn( +[\d|\.|\+|\-|e]+)( +[\d|\.|\+|\-|e]+)( +[\d|\.|\+|\-|e]+)/;
@@ -37,8 +36,9 @@ if(filenames.length === 2) {
     linenum = 0;
 
     var lines = fs.readFileSync(i).toString().split("\n");
-    var geometry = new Geometry(0);
     var material = '';
+
+    var mesh = null;
 
     lines.forEach(function(line) {
 
@@ -48,7 +48,7 @@ if(filenames.length === 2) {
             return;
 
         } else if((result = vertex_pattern.exec(line)) !== null) {
-            geometry.vertices.push(new Vector3(
+            model.vertices.push(new Vector3(
                 parseFloat(result[1]),
                 parseFloat(result[2]),
                 parseFloat(result[3])
@@ -56,7 +56,7 @@ if(filenames.length === 2) {
             vertices++;
 
         } else if((result = normal_pattern.exec(line)) !== null) {
-            geometry.normals.push(new Vector3(
+            model.normals.push(new Vector3(
                 parseFloat(result[1]),
                 parseFloat(result[2]),
                 parseFloat(result[3])
@@ -64,35 +64,38 @@ if(filenames.length === 2) {
             normals++;
 
         } else if((result = uv_pattern.exec(line)) !== null) {
-            geometry.uvs.push(new Vector3(
+            model.uvs.push(new Vector3(
                 parseFloat(result[1]),
                 parseFloat(result[2])
             ));
             uvs++;
 
         } else if((result = face_pattern3.exec(line)) !== null) {
-            geometry.faces.push(new Face(0,
+            mesh.faces.push(new Face(0,
                 [ parseInt(result[2])-1, parseInt(result[6])-1, parseInt(result[10])-1 ],
                 [ parseInt(result[3])-1, parseInt(result[7])-1, parseInt(result[11])-1 ],
                 [ parseInt(result[4])-1, parseInt(result[8])-1, parseInt(result[12])-1 ]
             ));
             faces++;
+        } else if(/^g /.test(line)) {
+            if(mesh) {
+                model.meshes.push(mesh);
+            }
+            mesh = new Mesh(line.substring(2).trim());
+            mesh.material = null;
 
         } else if(/^usemtl /.test(line)) {
-            var mesh = new Mesh(geometry);
-            mesh.material = material;
-            model.meshes.push(mesh);
-            geometry = new Geometry(0);
-
-            material = line.substring(7);
-
+            if(mesh.material !== null) {
+                model.meshes.push(mesh);
+                mesh = new Mesh(mesh.name);
+            }
+            mesh.material = line.substring(7).trim();
         } else {
             console.log('Unhandled: ' + linenum + ': ' + line);
         }
     });
 
-
-    var mesh = new Mesh(geometry);
+    //add last mesh
     model.meshes.push(mesh);
 
     var wstream = fs.createWriteStream(o);
